ed685d088d2ed3a34625722e02fcdb68490f3091
Lonami Exo <totufals@hotmail.com>
2017-04-22 11:57:38 +0200

    Generalize helperassign() to support any other operator

--- utils.py
+++ utils.py
@@ -37,130 +37,7 @@ def helperassign(c, dst, src):
 
     # Single assignment with support for mixed sizes (8 and 16)
     if len(dst) == 1:
-        dst = dst[0]
-        src = src[0]
-
-        # Sanity check: destination cannot be an inmediate value
-        if dst.value is not None:
-            raise ValueError(f'Cannot assign "{src.name}" to the inmediate value "{dst.name}"')
-
-        # If the source has an integer value, make sure it fits on destination
-        if src.value is not None:
-            if src.size > dst.size:
-                raise ValueError(f'"{src.name}" is too big to fit in "{dst.name}"')
-
-            # It's an okay inmediate value, simply assign it and early exit
-            c.add_code(f'mov {dst}, {src}')
-            return
-
-        # Special case where both are memory, we need to use a temporary
-        # register ('ax' for instance); recursive calls to helperassign()
-        # will then take care of the cases where assigning memory + 'ax'
-        if dst.is_mem and src.is_mem:
-            # # # [Case memory to memory]
-            # TODO This will have strange results when size dst > size src,
-            #      since no masking will be performed on 'ax'
-            c.add_code('push ax')
-            helperassign(c, 'ax', src)
-            helperassign(c, dst, 'ax')
-            c.add_code('pop ax')
-            return
-
-        # Now that we know the size, and that not both are memory,
-        # we can get our hands dirty
-        if dst.size == src.size:
-            # # # [Case same size]
-            # Both sizes are the same, we can directly move the values
-            # This should be the only move out there, if other moves
-            # are required below they should recursively call helperassign()
-            # to optimize this line away if 'dst' and 'src' are the same
-            c.add_code(f'mov {dst}, {src}')
-
-        elif dst.size < src.size:
-            # The destination is smaller, we have to ignore the high part
-            if src.is_mem or src.low is None:
-                # # # [Case large memory/register to small register]
-                # The source is memory, then the destination is a register
-                #
-                # We need an auxiliary register because we don't want to
-                # touch the other part (either of r'[HL]'), and we can't
-                # pop a masked value not to lose the rest
-                #
-                # This is also the case when the source is not memory, but
-                # the register doesn't support accessing r'[HL]'
-                #
-                # Use either 'dx' or 'ax' as auxiliar register (arbitrary
-                # as long as it doesn't match the one we want to move to)
-                aux = 'dx' if dst[0] == 'a' else 'ax'
-                c.add_code(f'push {aux}')
-                helperassign(c, aux, src)
-                helperassign(c, dst, f'{aux[0]}l')
-                c.add_code(f'pop {aux}')
-            else:
-                # # # [Case large register to small register/memory]
-                # We know that we have acces to the low part since it was
-                # checked above, otherwise we would have needed that
-                # auxiliary to be able to pick only the low part
-                helperassign(c, dst, f'{src.low}')
-
-        else:
-            # The destination is larger, we need to mask the high part.
-            # The only case where this is possible is on r'[ABCD][HL]',
-            # so we already know that we have access to the 'X' version
-            # unless the source is memory
-            if src.is_mem:
-                # # # [Case small memory to large register]
-                # Check if the register supports accessing to the r'[HL]'
-                # Otherwise we need to use a temporary register such as 'ax'
-                if dst.low is None:
-                    c.add_code(f'push ax')
-                    c.add_code(f'xor ah, ah')
-                    helperassign(c, 'al', src)
-                    helperassign(c, dst, 'ax')
-                    c.add_code(f'pop ax')
-                else:
-                    c.add_code(f'xor {dst.high}, {dst.high}')
-                    helperassign(c, f'{dst.low}', src)
-            elif dst.is_mem:
-                # # # [Case small register to large memory]
-                # All the 8 bit registers support accessing to 'X', so we
-                # can just mask away the high part, move it and restore
-                c.add_code(f'push {src.full}')
-
-                # We might need to move the high to the low part before masking
-                if src[-1] == 'h':
-                    helperassign(c, f'{src.low}', f'{src.high}')
-                c.add_code(f'xor {src.high}, {src.high}')
-
-                helperassign(c, dst, f'{src.full}')
-                c.add_code(f'pop {src.full}')
-            else:
-                # # # [Case small register to large register]
-                if src[-1] == 'l':
-                    # We're using the low part, we can directly copy
-                    # and then mask the high part with AND or XOR
-                    helperassign(c, dst, f'{src.full}')
-                    if dst.high is None:
-                        c.add_code(f'and {dst}, 0xff')
-                    else:
-                        c.add_code(f'xor {dst.high}, {dst.high}')
-                else:
-                    # We want to assign the src = YH, but it's 8-bits
-                    if dst.low is None:
-                        # No support to move the 8-bits directly, we need
-                        # to save the value, move it, mask it, and move it
-                        c.add_code(f'push {src.full}')
-                        helperassign(c, f'{src.low}', src)
-                        c.add_code(f'xor {src}, {src}')
-                        helperassign(c, dst, f'{src.full}')
-                        c.add_code(f'pop {src.full}')
-                    else:
-                        # Destination supports 8-bits access so we can
-                        # directly move those and mask away the high part
-                        helperassign(c, f'{dst.low}', src)
-                        c.add_code(f'xor {dst.high}, {dst.high}')
-
-        # Single assignment done, early exit
+        helperoperate(c, 'mov', dst[0], src[0])
         return
 
     # Multiple assignment
@@ -224,6 +101,135 @@ def helperassign(c, dst, src):
             pushed.pop()
         else:
             helperassign(c, dstv, srcv)
+
+
+def helperoperate(c, op, dst, src):
+    """Helper operation with support to operate on any value
+        (memory or not) of the form 'instruction dst, src'
+    """
+    if not isinstance(dst, Operand):
+        dst = Operand(c, dst)
+
+    if not isinstance(src, Operand):
+        src = Operand(c, src)
+
+    # Sanity check: destination cannot be an inmediate value
+    if dst.value is not None:
+        raise ValueError(f'Cannot {op} "{src.name}" to the inmediate value "{dst.name}"')
+
+    # If the source has an integer value, make sure it fits on destination
+    if src.value is not None:
+        if src.size > dst.size:
+            raise ValueError(f'"{src.name}" is too big to fit in "{dst.name}"')
+
+        # It's an okay inmediate value, simply operate and early exit
+        c.add_code(f'{op} {dst}, {src}')
+        return
+
+    # Special case where both are memory, we need to use a temporary
+    # register ('ax' for instance); recursive calls to helperoperate()
+    # will then take care of the cases where operating with memory + 'ax'
+    if dst.is_mem and src.is_mem:
+        # # # [Case memory to memory]
+        # TODO Perhaps we could use a temporary variable instead of the
+        #      stack to pick either AL or AX (which is better suited)
+        c.add_code('push ax')
+        helperassign(c, 'ax', src)
+        helperoperate(c, op, dst, 'ax')
+        c.add_code('pop ax')
+        return
+
+    # Now that we know the size, and that not both are memory,
+    # we can get our hands dirty
+    if dst.size == src.size:
+        # # # [Case same size]
+        # Both sizes are the same, we can directly operate (unless 'mov')
+        if op != 'mov' or dst != src:
+            c.add_code(f'{op} {dst}, {src}')
+
+    elif dst.size < src.size:
+        # The destination is smaller, we have to ignore the high part
+        if src.is_mem or src.low is None:
+            # # # [Case large memory/register to small register]
+            # The source is memory, then the destination is a register
+            #
+            # We need an auxiliary register because we don't want to
+            # touch the other part (either of r'[HL]'), and we can't
+            # pop a masked value not to lose the rest
+            #
+            # This is also the case when the source is not memory, but
+            # the register doesn't support accessing r'[HL]'
+            #
+            # Use either 'dx' or 'ax' as auxiliar register (arbitrary
+            # as long as it doesn't match the one we want to move to)
+            aux = 'dx' if dst[0] == 'a' else 'ax'
+            c.add_code(f'push {aux}')
+            helperassign(c, aux, src)
+            helperoperate(c, op, dst, f'{aux[0]}l')
+            c.add_code(f'pop {aux}')
+        else:
+            # # # [Case large register to small register/memory]
+            # We know that we have acces to the low part since it was
+            # checked above, otherwise we would have needed that
+            # auxiliary to be able to pick only the low part
+            helperoperate(c, op, dst, src.low)
+
+    else:
+        # The destination is larger, we need to mask the high part.
+        # The only case where this is possible is on r'[ABCD][HL]',
+        # so we already know that we have access to the 'X' version
+        # unless the source is memory
+        if src.is_mem:
+            # # # [Case small memory to large register]
+            # Check if the register supports accessing to the r'[HL]'
+            # Otherwise we need to use a temporary register such as 'ax'
+            if dst.low is None:
+                c.add_code(f'push ax')
+                c.add_code(f'xor ah, ah')
+                helperassign(c, 'al', src)
+                helperoperate(c, op, dst, 'ax')
+                c.add_code(f'pop ax')
+            else:
+                c.add_code(f'xor {dst.high}, {dst.high}')
+                helperoperate(c, op, dst.low, src)
+        elif dst.is_mem:
+            # # # [Case small register to large memory]
+            # All the 8 bit registers support accessing to 'X', so we
+            # can just mask away the high part, move it and restore
+            c.add_code(f'push {src.full}')
+
+            # We might need to move the high to the low part before masking
+            if src[-1] == 'h':
+                helperassign(c, src.low, src.high)
+            c.add_code(f'xor {src.high}, {src.high}')
+
+            helperoperate(c, op, dst, src.full)
+            c.add_code(f'pop {src.full}')
+        else:
+            # # # [Case small register to large register]
+            if src[-1] == 'l':
+                # We're using the low part, we can directly copy
+                # and then mask the high part with AND or XOR
+                helperoperate(c, op, dst, src.full)
+                if dst.high is None:
+                    c.add_code(f'and {dst}, 0xff')
+                else:
+                    c.add_code(f'xor {dst.high}, {dst.high}')
+            else:
+                # We want to assign the src = YH, but it's 8-bits
+                if dst.low is None:
+                    # No support to move the 8-bits directly, we need
+                    # to save the value, move it, mask it, and move it
+                    c.add_code(f'push {src.full}')
+                    helperassign(c, src.low, src)
+                    c.add_code(f'xor {src}, {src}')
+                    helperoperate(c, op, dst, src.full)
+                    c.add_code(f'pop {src.full}')
+                else:
+                    # Destination supports 8-bits access so we can
+                    # directly move those and mask away the high part
+                    helperoperate(c, op, dst.low, src)
+                    c.add_code(f'xor {dst.high}, {dst.high}')
 
 
 def parseint(value):
