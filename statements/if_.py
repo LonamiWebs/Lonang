8ce5d19d839edfaa00fe7cc3220a398dd77502b2
Lonami Exo <totufals@hotmail.com>
2017-04-22 21:41:28 +0200

    Improve if condition to support "is even" and "is odd"

--- statements/if_.py
+++ statements/if_.py
@@ -7,16 +7,32 @@ def if_(c, m):
         if ax < 5 {
             ; code
         }
+
+        if cx is even {
+            ; code
+        }
     """
-    label = c.get_uid(m.group(4))
-    c.add_code([
-        f'cmp {m.group(1)}, {m.group(3)}',
-        f'{ctoi[m.group(2)]} {label}'
-    ])
+    even_odd = m.group(4)
+    label = c.get_uid(m.group(5))
+
+    if even_odd is None:
+        c.add_code([
+            f'cmp {m.group(1)}, {m.group(3)}',
+            f'{ctoi[m.group(2)]} {label}'
+        ])
+    else:
+        # If even, then bit is 0, then jump to skip if not zero
+        # If  odd, then bit is 1, then jump to skip if zero
+        jump = 'jnz' if even_odd == 'even' else 'jz'
+        c.add_code([
+            f'test {m.group(1)}, 1',
+            f'{jump} {label}'
+        ])
+
     c.add_pending_code(f'{label}:')
 
 
 if_statement = Statement(
-    r'if  (VAR) ([<>=!]+) (INM) {',
+    r'if  (VAR)(?: ([<>=!]{2}) (INM)|  is  (even|odd)) {',
     if_
 )
