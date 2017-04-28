2022c5c46047355c5890bf703e98a44721769287
Lonami Exo <totufals@hotmail.com>
2017-04-28 18:32:01 +0200

    Create a Machine emulator class (not working)

---
+++ emu/machine.py
@@ -0,0 +1,206 @@
+from parser import parseint, parse_memory_addr
+from parser import pack_value, unpack_value
+
+
+class Machine:
+    """Represents an 8086 machine"""
+    def __init__(self):
+        # TODO We actually don't care about DS value, but we should
+        self.memory = []
+
+        # Name and location for certain indices (and type size) in memory
+        #   name: (index, size)
+        self.memory_nameloc = {}
+
+        # TODO Infinite stack, not actually what was reserved
+        self.stack = []
+
+        # Machine registers (general purpose, instruction pointer, flags...)
+        self.registers = {
+            n: 0 for n in 'ax bx cx dx cs ip ss sp bp si di ds es'.split()
+        }
+        self.flags = {
+            f: False for f in 'cf zf sf of pf af if df'.split()
+        }
+
+        # Machine labels used on jumps TODO Doesn't sound like a good idea
+        self.labels = {}
+
+
+    def _isregister(self, name):
+        """Returns True if the given 'name' is a register"""
+        name = name.lower()
+        if name[-1] in 'xhl':
+            return f'{name[0]}x' in self.registers
+        else:
+            return name in self.registers or name in self.flags
+
+
+    def tag_memory(self, name, size):
+        """Tags the current memory size with the given name and size"""
+        self.memory_nameloc[name] = len(self.memory), size
+
+
+    def get_memory_addr(self, tag_name):
+        """Gets the memory index for the given tag name"""
+        return self.memory_nameloc[tag_name][0]
+
+
+    def add_to_memory(self, values):
+        """Adds the given value(s) to memory"""
+        if isinstance(values, list):
+            self.memory.extend(values)
+        else:
+            self.memory.append(value)
+
+
+    def update_flags(self, result, size):
+        """Updates the flags state when 'result'
+           happens on 'size' operands
+        """
+        # Carry flag (CF)
+        # Parity flag (PF)
+        # Auxiliary carry flag (AF)
+        # Zero flag (ZF)
+        # Sign flag (SF)
+        # Trap flag (TF)
+        # Interrupt flag (IF)
+        # Direction flag (DF)
+        # Overflow flag (OF)
+        # TODO flags: AF; TF; IF; DF
+        # TODO size = max(access_size(op1), access_size(op2))
+        self.flags['cf'] = result >= 2**size
+        self.flags['zf'] = result == 0
+        self.flags['sf'] = (result & (1 << (size - 1))) != 0
+
+        r = result
+        self.flags['pf'] = True
+        while r > 0:
+            if r & 1 == 1:
+                self.flags['pf'] = not self.flags['pf']
+            r >>= 1
+
+        # A negative result out of positive operands (or vice versa) is an overflow
+        power = 2**size
+        if result < 0:
+            comp = ((result ^ (power - 1)) + 1) & power - 1
+        else:
+            comp = result
+
+        self.flags['of'] = comp < 0 or comp >= 2**(size-1)
+
+
+    def push(self, value):
+        """Pushes the given value to the stack"""
+        self.stack.append(value)
+
+
+    def pop(self):
+        """Pops a value from the machine's stack"""
+        return self.stack.pop()
+
+
+    def __getitem__(self, name):
+        """Accesses to the given name (register, memory...)"""
+        if not name:
+            return 0
+
+        # TODO Maybe from "parser" import parsint, like code parser...
+        #      or better parseexpr
+        value = parseint(name)
+        if value is not None:
+            return value
+
+        if self._isregister(name):
+            name = name.lower()
+            if name[-1] == 'f':
+                return self.flags[name]
+            elif name[-1] == 'h':
+                name = f'{name[0]}x'
+                return self.registers[name] >> 8
+            elif name[-1] == 'l':
+                name = f'{name[0]}x'
+                return self.registers[name] & 0xff
+            else:
+                return self.registers[name]
+        else:
+            if name.lower() == 'data':
+                # TODO Not actually returning the location of DS
+                return 0
+
+            if '[' in name:
+                # TODO Seems like indirect memory access is treated as 8 bits
+                idx = parse_memory_addr(self, name)
+                size = 8
+                take = 1
+            else:
+                idx, size = self.memory_nameloc[name]
+                take = size // 8
+
+            mem = self.memory[idx:idx+take]
+            return unpack_value(mem, size)
+
+
+    def __setitem__(self, name, value):
+        """Accesses to the given name (register, memory...)"""
+        # TODO Assert it doesn't access invalid sizes
+        if self._isregister(name):
+            if name[-1] == 'f':
+                self.flags[name] = value
+
+            if name[-1] == 'h':
+                name = f'{name[0]}x'
+                result = (self.registers[name] & 0x00ff) | (value << 8)
+                self.registers[name] = result
+
+            elif name[-1] == 'l':
+                name = f'{name[0]}x'
+                result = (self.registers[name] & 0xff00) |  value
+                self.registers[name] = result
+
+            else:
+                self.registers[name] = value
+
+        else:
+            if '[' in name:
+                # TODO Seems like indirect memory access is treated as 8 bits
+                idx = parse_memory_addr(self, name)
+                size = 8
+                take = 1
+            else:
+                idx, size = self.memory_nameloc[name]
+                take = size // 8
+
+            self.memory[idx:idx+take] = pack_value(value, size)
+
+    def sizeof(self, name):
+        """Determines the size of the given name"""
+        if not name:
+            return 0
+
+        value = parseint(name)
+        if value is not None:
+            if value.bit_length() <= 8:
+                return 8
+            if value.bit_length() <= 16:
+                return 16
+
+        if self._isregister(name):
+            name = name.lower()
+            if name[-1] == 'f':
+                return 1
+            elif name[-1] in 'hl':
+                return 8
+            else:
+                return 16
+        else:
+            if '[' in name:
+                name = name[:name.index('[')]
+                if not name:
+                    return 8
+
+            _, size = self.memory_nameloc[name]
+            return size
+
+        print(f'err: could not determine size of {name}')
+        quit()
---
+++ emu/parser.py
@@ -0,0 +1,148 @@
+import struct
+
+
+def stripcomments(line):
+    """Strips the comments from 'line'"""
+    # TODO strip comments in a nicer way
+    semicolon = line.find(';')
+    if semicolon != -1:
+        line = line[:semicolon]
+
+    return line.strip()
+
+
+def pack_value(v, size):
+    # Determine the pack type
+    if size == 8:
+        pt = '<b' if v < 0 else '<B'
+    elif size == 16:
+        pt = '<h' if v < 0 else '<H'
+    else:
+        print(f'err: invalid size for packing a value')
+        quit()
+
+    return struct.pack(pt, v)
+
+
+def unpack_value(packed, size):
+    # Determine the pack type TODO How do we support negative values?
+    if size == 8:
+        pt = '<B'
+    elif size == 16:
+        pt = '<H'
+    else:
+        print(f'err: invalid size for unpacking a value')
+        quit()
+
+    return struct.unpack(pt, bytes(packed))[0]
+
+
+# TODO Copy-paste from operands.py (except for the isinstance part)
+def parseint(value):
+    """Tries parsing an integer value, returns None on failure"""
+    if isinstance(value, int):
+        return value
+
+    if not value or not value.strip():
+        return None
+
+    value = value.strip()
+    try:
+        if value[0] == "'" and value[-1] == "'":
+            value = value.strip("'").encode('ascii').decode('unicode_escape')
+            if len(value) != 1:
+                print('err: character can only be length 1')
+                quit()
+
+            return ord(value[0])
+
+        value = value.lower()
+        if value.startswith('0x'):
+            return int(value[2:], base=16)
+
+        if value[0].isdigit() and value[-1] == 'h':
+            return int(value[:-1], base=16)
+
+        if value.startswith('0b'):
+            return int(value[2:], base=2)
+
+        if value[0].isdigit() and value[-1] == 'b':
+            return int(value[:-1], base=2)
+
+        return int(value)
+    except ValueError:
+        return None
+
+
+def parsevalues(value, size):
+    """Converts a given value, such a string or
+       a vector, into a vector of byte values.
+
+       'size' must be either 8 or 16
+    """
+    # TODO Allow binary values to be specified, do some parseint
+    result = []
+    values = [stripcomments(v) for v in value.split(',') if stripcomments(v)]
+    for v in values:
+        if 'dup(' in v.lower() and v[-1] == ')':
+            count, dup = v.split()
+            count = parseint(count)
+            dup = dup[len('dup('):-len(')')]
+            if dup == '?':
+                result.extend(0 for _ in range(count))
+            else:
+                dup = parseint(dup)
+                result.extend(dup for _ in range(count))
+
+        elif v[0] == '"' and v[-1] == '"':
+            result.extend(ord(c) for c in v[1:-1])
+
+        elif v[0] == "'" and v[-1] == "'":
+            result.append(ord(v[1]))
+
+        elif v == '?':
+            result.append(0)
+
+        else:
+            if parseint(v) is None:
+                print(f'err: could not parse {v} as integer')
+                quit()
+            result.append(parseint(v))
+
+    return [v for r in result for v in pack_value(r, size)]
+
+
+def parse_memory_addr(m, addr):
+    """Parses a memory address, like [bx+si+7]"""
+    # Supporting addresses like '[bx][si]' -> bx+si'
+    #                            ^ skip ^
+    #
+    # TODO Can I actually transform the base into something inside parenthesis?
+    first = addr.find('[')
+    base, addr = addr[:first], addr[first:]
+    if base:
+        accum = m[base]
+    else:
+        accum = 0
+
+    addr = addr[1:-1] \
+                .strip() \
+                .replace(']', '') \
+                .replace('[', '+')
+
+    # We can't have a starting operator, so prepend 0 if this is the case
+    if addr[0] in '+-':
+        addr = f'0{addr}'
+
+    # Now determine operators and split on them
+    operators = [c for c in addr if c in '+-']
+    operands = addr.replace('-', '+').split('+')
+
+    # Now operate
+    accum += m[operands[0]]
+    adding = True
+    for i in range(1, len(operands)):
+        multiplier = 1 if operators[i-1] == '+' else -1
+        accum += multiplier * m[operands[i]]
+
+    return accum
